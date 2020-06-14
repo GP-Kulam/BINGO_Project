e8df4323d6561de0a85987cfc68f769a1e837a37
GP Kulam <kulamgangadhar03@gmail.com>
2020-06-14 17:10:16 +0530

    Added about button and the help frame is cretaed

--- src/Bingo/FirstFrame.java
+++ src/Bingo/FirstFrame.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package Bingo;
-import Bingo.NewGameFrame.*;
+import Bingo.*;
 import static javafx.application.Platform.exit;
 
 /**
@@ -41,6 +41,7 @@ public class FirstFrame extends javax.swing.JFrame {
         StartButton = new javax.swing.JButton();
         ResumeButton = new javax.swing.JButton();
         HelpButton = new javax.swing.JButton();
+        AboutButton = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
 
@@ -57,23 +58,23 @@ public class FirstFrame extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Yu Gothic Medium", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Welcome To world of Games");
+        jLabel1.setText("Welcome To The world of Games");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(85, Short.MAX_VALUE)
+                .addContainerGap(65, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addGap(79, 79, 79))
+                .addGap(59, 59, 59))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jLabel2.setFont(new java.awt.Font("MV Boli", 1, 48)); // NOI18N
@@ -130,6 +131,17 @@ public class FirstFrame extends javax.swing.JFrame {
             }
         });
 
+        AboutButton.setBackground(new java.awt.Color(0, 204, 0));
+        AboutButton.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        AboutButton.setForeground(new java.awt.Color(255, 255, 255));
+        AboutButton.setText("About ");
+        AboutButton.setName("Help"); // NOI18N
+        AboutButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AboutButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -152,8 +164,9 @@ public class FirstFrame extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(StartButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(ResumeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(HelpButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(123, 123, 123))
+                    .addComponent(HelpButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(AboutButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(131, 131, 131))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -173,7 +186,9 @@ public class FirstFrame extends javax.swing.JFrame {
                 .addComponent(ResumeButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(HelpButton)
-                .addContainerGap(99, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(AboutButton)
+                .addContainerGap(55, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -200,7 +215,7 @@ public class FirstFrame extends javax.swing.JFrame {
 
     private void HelpButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HelpButtonActionPerformed
         // TODO add your handling code here:
-        
+        new HelpFrame().setVisible(true);
     }//GEN-LAST:event_HelpButtonActionPerformed
 
     private void StartButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StartButtonActionPerformed
@@ -209,6 +224,11 @@ public class FirstFrame extends javax.swing.JFrame {
         this.setVisible(false);
         
     }//GEN-LAST:event_StartButtonActionPerformed
+
+    private void AboutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AboutButtonActionPerformed
+        // TODO add your handling code here:
+        new AboutFrame().setVisible(true);
+    }//GEN-LAST:event_AboutButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -246,6 +266,7 @@ public class FirstFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AboutButton;
     private javax.swing.JButton HelpButton;
     private javax.swing.JButton ResumeButton;
     private javax.swing.JButton StartButton;
